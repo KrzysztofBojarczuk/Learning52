59f88a6f51a71f9fe47330f8428fca3daffe7efd
KrzysztofBojarczuk <69596796+KrzysztofBojarczuk@users.noreply.github.com>
2021-04-11 07:46:50 +0200

    Add project files.

---
+++ przevyta odległość/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace przevyta_odległość
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double odgległość = 0;
+            Console.WriteLine("Z jaką prędkością jechał pojazd?");
+            double speed = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Jak długo trwała podróż?");
+            int hours = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Godzina              Przebyta odległość");
+            Console.WriteLine("---------------------------------------");
+
+            for (int  i = 1; i<= hours; i++)
+            {
+                
+                odgległość = speed * i;
+                Console.WriteLine($"{i}                {odgległość}");
+            }
+        }
+    }
+}
